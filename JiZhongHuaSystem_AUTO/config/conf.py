e6225273cdd977d87391a78814d75d1746e20dce
kingwohhh <95902252+kingwohhh@users.noreply.github.com>
2025-02-28 09:47:29 +0800

    Delete JiZhongHuaSystem_AUTO directory

--- JiZhongHuaSystem_AUTO/config/conf.py
+++
@@ -1,4 +0,0 @@
-class ConfigManager:
-    PROJECT_URL = "https://dev.jiandaoyun.com/signin"
-    USERNAME = "15334510271"
-    PASSWORD = "Test666666"
--- JiZhongHuaSystem_AUTO/testcases/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture(scope='session', autouse=True)
-def drivers():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.quit()
